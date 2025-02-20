d957d3e4f1ba2732592c233ccde9bbb41088e66d
Gerrit Balindt <gerrit.balindt@gruenecho.de>
2025-02-20 12:04:54 +0100

    test 15000 cluster

--- supabase/seeding.sh
+++ supabase/seeding.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# Set the chunk size (e.g., 50MB = 50000000 bytes)
+CHUNK_SIZE=50000
+
 #set -e  # Exit on error
 set -a && source ../.env.local && set +a
 
@@ -13,6 +16,10 @@ clean_sql_file() {
     sed -i '' '/SET transaction_timeout/d' "$file"
     echo "SET session_replication_role = replica;" | cat - "$file" > temp && mv temp "$file"
 }
+
+# Remove all sql files in folder
+rm -f seeds/public/*.sql
+rm -f seeds/hidden/*.sql
 
 # Dump lookup schema
 PGPASSWORD=$POSTGRES_PASSWORD pg_dump \
@@ -38,11 +45,11 @@ for table in cluster plot deadwood edges regeneration structure_lt4m tree
         -n inventory_archive \
         --table inventory_archive.$table \
         --inserts \
-        --rows-per-insert=10000 \
         --data-only \
         --enable-row-security \
         > seeds/public/$table.sql
     clean_sql_file seeds/public/$table.sql
+
 done;
 
 for table in plot_coordinates plot_landmark position subplots_relative_position
@@ -54,9 +61,34 @@ for table in plot_coordinates plot_landmark position subplots_relative_position
         -n inventory_archive \
         --table inventory_archive.$table \
         --inserts \
-        --rows-per-insert=10000 \
         --data-only \
         --enable-row-security \
         > seeds/hidden/$table.sql
     clean_sql_file seeds/hidden/$table.sql
+
 done;
+
+
+# make directory if it doesn't exist
+mkdir -p seeds/public/plot
+# split into smaller files
+split -l $CHUNK_SIZE seeds/public/plot.sql seeds/public/plot_part_
+# Add necessary PostgreSQL headers to each chunk
+for file in seeds/public/plot_part_*; do
+    clean_sql_file $file
+    mv "$file" "$file.sql"
+done;
+# remove base file
+rm seeds/public/plot.sql
+
+# make directory if it doesn't exist
+mkdir -p seeds/public/tree
+# split into smaller files
+split -l $CHUNK_SIZE seeds/public/tree.sql seeds/public/tree_part_
+# Add necessary PostgreSQL headers to each chunk
+for file in seeds/public/tree_part_*; do
+    clean_sql_file $file
+    mv "$file" "$file.sql"
+done;
+# remove base file
+rm seeds/public/tree.sql
