97321a8dc8d55dea402c8520ddada6f5b3835d8f
sebschnell <sebastian.schnell@thuenen.de>
2026-08-31 09:39:19 +0200

    Update 20260831000000_workflow_status_beta_names.sql
    Anpassung name_de und name_en

--- supabase/migrations/20260831000000_workflow_status_beta_names.sql
+++ supabase/migrations/20260831000000_workflow_status_beta_names.sql
@@ -3,67 +3,67 @@ SET search_path TO lookup;
 -- Code 10 -- Initialisierung, Zuweisung LIL
 UPDATE lookup.lookup_workflow_status SET
     name_de = 'Vorarbeiten BIL (beta)',
-    name_en = 'Preparation NFI management (beta)'
+    name_en = 'Preparatory work federal administration (beta)'
 WHERE code = 10;
 
 -- Code 20 -- Anlage Dienstleister und Trupps, Zuweisung Aufnahmetrupp
 UPDATE lookup.lookup_workflow_status SET
     name_de = 'Vorarbeiten LIL (beta)',
-    name_en = 'Preparation state management (beta)'
+    name_en = 'Preparatory work state administration (beta)'
 WHERE code = 20;
 
 -- Code 31 -- Erstaufnahme durch den Aufnahmetrupp
 UPDATE lookup.lookup_workflow_status SET
-    name_de = 'Aufnahme im Feld AT (beta)',
-    name_en = 'Field survey (beta)'
+    name_de = 'Erstaufnahme AT (beta)',
+    name_en = 'Initial data collection field team (beta)'
 WHERE code = 31;
 
 -- Code 32 -- Wiederholung oder Korrektur durch den Aufnahmetrupp
 UPDATE lookup.lookup_workflow_status SET
-    name_de = 'Aufnahme im Feld AT (beta)',
-    name_en = 'Repeat survey (beta)'
+    name_de = 'Wiederholung/Korrektur AT (beta)',
+    name_en = 'Revision/repetition by field team (beta)'
 WHERE code = 32;
 
 -- Code 41 -- Feldaufnahme beendet, Pruefung durch die LIL
 UPDATE lookup.lookup_workflow_status SET
-    name_de = 'Qualitaetskontrolle LIL (beta)',
-    name_en = 'Quality control state (beta)'
+    name_de = 'Kontrolle LIL nach AT Erstaufnahme (beta)',
+    name_en = 'Data check state administration after initial field work (beta)'
 WHERE code = 41;
 
--- Code 42 -- Pruefung nach Korrektur oder Wiederholungsaufnahme
+-- Code 42 -- Prüfung nach Korrektur oder Wiederholungsaufnahme
 UPDATE lookup.lookup_workflow_status SET
-    name_de = 'Qualitaetskontrolle LIL (beta)',
-    name_en = 'Quality control state (beta)'
+    name_de = 'Kontrolle LIL nach AT Korrektur/Wiederholung (beta)',
+    name_en = 'Data check state administration after repetition (beta)'
 WHERE code = 42;
 
--- Code 43 -- Pruefung nach Einsatz des Kontrolltrupps
+-- Code 43 -- Prüfung nach Einsatz des Kontrolltrupps
 UPDATE lookup.lookup_workflow_status SET
-    name_de = 'Qualitaetskontrolle LIL (beta)',
-    name_en = 'Quality control state (beta)'
+    name_de = 'Kontrolle LIL nach KT (beta)',
+    name_en = 'Data check state administration after control team (beta)'
 WHERE code = 43;
 
--- Code 44 -- Pruefung nach Rueckgabe durch die BIL
+-- Code 44 -- Prüfung nach Rückgabe durch die BIL
 UPDATE lookup.lookup_workflow_status SET
-    name_de = 'Qualitaetskontrolle LIL (beta)',
-    name_en = 'Quality control state (beta)'
+    name_de = 'Kontrolle LIL nach Rückgabe BIL (beta)',
+    name_en = 'Data check state administration after rejection by federal administration (beta)'
 WHERE code = 44;
 
 -- Code 50 -- Kontrolltrupp erhebt Daten
 UPDATE lookup.lookup_workflow_status SET
-    name_de = 'Kontrolle/Korrektur im Feld (beta)',
-    name_en = 'Control survey (beta)'
+    name_de = 'Aufnahme KT (beta)',
+    name_en = 'Data collection by control team (beta)'
 WHERE code = 50;
 
--- Code 60 -- Von der LIL akzeptiert, Pruefung durch die BIL
+-- Code 60 -- Von der LIL akzeptiert, Prüfung durch die BIL
 UPDATE lookup.lookup_workflow_status SET
-    name_de = 'Qualitaetskontrolle BIL (beta)',
-    name_en = 'Quality control NFI (beta)'
+    name_de = 'Kontrolle BIL (beta)',
+    name_en = 'Data check federal administration (beta)'
 WHERE code = 60;
 
 -- Code 70 -- Ecke abgeschlossen
 UPDATE lookup.lookup_workflow_status SET
     name_de = 'Akzeptiert BIL (beta)',
-    name_en = 'Accepted (beta)'
+    name_en = 'Accepted by federal administration (beta)'
 WHERE code = 70;
 
 COMMENT ON TABLE lookup.lookup_workflow_status IS
